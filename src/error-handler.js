00e6f96a96756a9d196e2a2cdb810ed933cfd53d
Alex <j@dytry.ch>
2016-04-19 09:55:40 +0100

    Custom error handler, include originating service name in error

---
+++ src/error-handler.js
@@ -0,0 +1,20 @@
+import statuses from 'statuses';
+
+const production = process.env.NODE_ENV === 'production';
+
+export default (serviceName) => (req, res, next, err) => {
+  const status = err.status || err.statusCode || 500;
+
+  res.status(status).json({
+    status,
+    stack: production && err.stack,
+    message: err.message || statuses[status],
+    code: err.code,
+    name: err.name,
+    type: err.type,
+    details: err.details,
+    cat: `https://http.cat/${status}`,
+    origin: serviceName,
+    requestId: req.headers['x-request-id'],
+  });
+};
