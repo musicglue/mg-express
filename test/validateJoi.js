50bd0e496de79041ef52c135e006455087836e7a
Stéphane Maniaci <stephane.maniaci@gmail.com>
2016-09-13 11:57:58 +0100

    feat: add validate joi

---
+++ test/validateJoi.js
@@ -0,0 +1,34 @@
+import chai, { expect } from 'chai';
+import dirtyChai from 'dirty-chai';
+import asPromised from 'chai-as-promised';
+
+import validate from '../src/validateJoi';
+
+chai.use(asPromised);
+chai.use(dirtyChai);
+
+describe('joi validation', () => {
+  context('when the schema is not valid', () => {
+    const schema = {
+      validate: (value, callback) => {
+        const err = new Error();
+        err.name = 'ValidationError';
+
+        return callback(err);
+      },
+    };
+
+    it('rejects', () => expect(validate(schema, null)).to.be.rejected());
+
+    it('sets the error code to 400', () => validate(schema, null)
+       .catch(e => expect(e.status).to.eq(400)));
+  });
+
+  context('when the schema is valid', () => {
+    const schema = {
+      validate: (value, callback) => callback(null, value),
+    };
+
+    it('resolves', () => expect(validate(schema, null)).to.be.fulfilled());
+  });
+});
