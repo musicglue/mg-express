3515160024272225ca6a6fa7ba45962ebac3ecf9
Niklas Lindgren <niklas@musicglue.com>
2016-09-27 18:11:49 +0100

    fix: ensure params are of valid type

--- src/methodMetrics.js
+++ src/methodMetrics.js
@@ -3,6 +3,11 @@ import * as metrics from './metrics';
 export default (instance, key, instanceName, methods) => {
   methods.forEach(methodName => {
     const originalMethod = instance[methodName];
+
+    if (originalMethod == null || typeof originalMethod !== 'function') {
+      throw new TypeError(`Expected ${instanceName}.${methodName} to be a function`);
+    }
+
     instance[methodName] = function tracked(...args) { // eslint-disable-line no-param-reassign
       const start = Date.now();
       const result = originalMethod.apply(this, args);
