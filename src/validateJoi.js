727b5b1cb2ad45360027acc67533d69519241206
Lee Henson <lee.m.henson@gmail.com>
2017-07-24 13:25:57 +0100

    feat: remove bluebird

--- src/validateJoi.js
+++ src/validateJoi.js
@@ -1,13 +1,16 @@
-import Bluebird from 'bluebird';
-
-global.Promise = Bluebird;
-
 const isValidationError = e => e.name === 'ValidationError';
 
 export default (schema, value, options = {}) =>
-  Promise
-    .fromCallback(cb => schema.validate(value, options, cb))
-    .catch(isValidationError, e => {
-      e.status = 400; // eslint-disable-line no-param-reassign
-      return Promise.reject(e);
+  new Promise((resolve, reject) => {
+    schema.validate(value, options, (err, validated) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(validated);
     });
+  })
+  .catch(isValidationError, e => {
+    e.status = 400; // eslint-disable-line no-param-reassign
+    return Promise.reject(e);
+  });
